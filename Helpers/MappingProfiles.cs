0bdad61940c038e545244f1ea8a11dddda7b8b52
hubedid <hubed@protonmail.com>
2023-08-20 11:10:23 +0700

    [ADD] new mapping helper

---
+++ Helpers/MappingProfiles.cs
@@ -0,0 +1,14 @@
+﻿using A_GroTech_Api.Dto;
+using A_GroTech_Api.Models;
+using AutoMapper;
+
+namespace A_GroTech_Api.Helpers
+{
+	public class MappingProfiles : Profile
+	{
+        public MappingProfiles()
+        {
+            CreateMap<User, UserDto>();
+        }
+    }
+}
