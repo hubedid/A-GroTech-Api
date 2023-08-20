e785a4f82c039e7ee5c3d9e65a12ee5a68b9fb4c
hubedid <hubed@protonmail.com>
2023-08-20 11:12:11 +0700

    [ADD] new user data transfer object

---
+++ Dto/UserDto.cs
@@ -0,0 +1,13 @@
+﻿namespace A_GroTech_Api.Dto
+{
+	public class UserDto
+	{
+		public string Id { get; set; }
+		public string Name { get; set; }
+		public string Email { get; set; }
+		public string Address { get; set; }
+		public string Picture { get; set; }
+		public string NotificationToken { get; set; }
+		public int Status { get; set; }
+	}
+}
