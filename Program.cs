ffc8ccb06d51d8910397a8196ce6ea6bcd4da787
hubedid <hubed@protonmail.com>
2023-08-23 08:23:13 +0700

    [ADD] Seeder configuration

--- Program.cs
+++ Program.cs
@@ -1,11 +1,16 @@
+using A_GroTech_Api;
 using A_GroTech_Api.Data;
+using A_GroTech_Api.Helpers;
 using A_GroTech_Api.Interfaces;
 using A_GroTech_Api.Models;
 using A_GroTech_Api.Repository;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,10 +18,47 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IDiscussionRepository, DiscussionRepository>();
+builder.Services.AddScoped<IDiscussionAnswerRepository, DiscussionAnswerRepository>();
+builder.Services.AddScoped<IAreaRepository, AreaRepository>();
+builder.Services.AddScoped<ResponseHelper>();
+builder.Services.AddTransient<Seed>();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+	c.SwaggerDoc("v1", new OpenApiInfo { Title = "A-GroTech Hoaksss", Version = "v1" });
+
+	// Konfigurasi untuk menambahkan header Bearer
+	var securityScheme = new OpenApiSecurityScheme
+	{
+		Name = "Authorization",
+		In = ParameterLocation.Header,
+		Type = SecuritySchemeType.Http,
+		Scheme = "Bearer",
+		BearerFormat = "JWT",
+		Description = "Enter 'token' for authorization"
+	};
+	c.AddSecurityDefinition("Bearer", securityScheme);
+
+	var securityRequirement = new OpenApiSecurityRequirement
+	{
+		{
+			new OpenApiSecurityScheme
+			{
+				Reference = new OpenApiReference
+				{
+					Type = ReferenceType.SecurityScheme,
+					Id = "Bearer"
+				}
+			},
+			new string[] {}
+		}
+	};
+	c.AddSecurityRequirement(securityRequirement);
+});
+
 builder.Services.AddDbContext<DataContext>(options => {
 	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
@@ -32,7 +74,12 @@ builder.Services.AddIdentity<User, IdentityRole>(options =>
 })
 	.AddEntityFrameworkStores<DataContext>()
 	.AddDefaultTokenProviders();
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+builder.Services.AddAuthentication(auth =>
+{
+	auth.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+	auth.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+	auth.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
 	.AddJwtBearer(options =>
 	{
 		options.TokenValidationParameters = new TokenValidationParameters
@@ -45,9 +92,37 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 			ValidAudience = builder.Configuration["Jwt:Audience"],
 			IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
 		};
+		options.Events = new JwtBearerEvents
+		{
+			OnChallenge = async context =>
+			{
+				context.HandleResponse();
+				context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+				context.Response.ContentType = "application/json";
+				var result = new UnauthorizedObjectResult(new ResponseHelper().Error("You are not authorized"));
+				await result.ExecuteResultAsync(new ActionContext
+				{
+					HttpContext = context.HttpContext
+				});
+			}
+		};
 	});
 
 var app = builder.Build();
+
+if (args.Length == 1 && args[0].ToLower() == "seeddata")
+	SeedData(app);
+
+async void SeedData(IHost app)
+{
+	var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+	using (var scope = scopedFactory.CreateScope())
+	{
+		var service = scope.ServiceProvider.GetService<Seed>();
+		await service.SeedDataContextAsync();
+	}
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -58,8 +133,56 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+/*app.UseExceptionHandler(errorApp =>
+{
+	errorApp.Run(async context =>
+	{
+		context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+		context.Response.ContentType = "application/json";
+
+		var exceptionHandlerPathFeature = context.Features.Get<IExceptionHandlerPathFeature>();
+		var errorMessage = exceptionHandlerPathFeature?.Error.Message;
+
+		// Cek apakah status code adalah 401 Unauthorized
+		if (context.Response.StatusCode == (int)HttpStatusCode.Unauthorized)
+		{
+			var unauthorizedResponse = new
+			{
+				Message = "You are not authorized to access this resource."
+			};
+
+			var responseBody = JsonSerializer.Serialize(unauthorizedResponse);
+			await context.Response.WriteAsync(responseBody);
+		}
+		else
+		{
+			var responseMessage = new
+			{
+				Message = "An error occurred while processing your request.",
+				Error = errorMessage
+			};
+
+			var responseBody = JsonSerializer.Serialize(responseMessage);
+			await context.Response.WriteAsync(responseBody);
+		}
+	});
+});*/
+/*app.Use(async (context, next) =>
+{
+	await next();
+	if (context.Response.StatusCode == 404)
+	{
+		context.Response.ContentType = "application/json";
+		var result = new NotFoundObjectResult(new ResponseHelper().Error("Not Found", 404));
+		await result.ExecuteResultAsync(new ActionContext
+		{
+			HttpContext = context.Response.HttpContext
+		});
+	}
+});*/
 app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+await app.RunAsync();
--- Seed.cs
+++ Seed.cs
@@ -1,6 +1,368 @@
-﻿namespace A_GroTech_Api
+﻿using A_GroTech_Api.Data;
+using A_GroTech_Api.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace A_GroTech_Api
 {
 	public class Seed
 	{
-	}
+		private readonly DataContext _context;
+		private readonly UserManager<User> _userManager;
+
+		public Seed(DataContext context, UserManager<User> userManager)
+        {
+			_context = context;
+			_userManager = userManager;
+		}
+		/* * * * * * Login Data * * * * * *
+		 * Email	: user@example.com	  *
+		 * Password	: walangkecek123	  *
+		 * * * * * * * * * * * * * * * * * */ 
+		public async Task SeedDataContextAsync()
+		{
+			/*if (_context.Users.Any())
+			{*/
+				var userNew = new List<User>()
+				{
+					new User
+					{
+						Name = "yahaha",
+						UserName = "tes123",
+						Email = "usertes1@example.com"
+					},
+					new User
+					{
+						Name = "hayukkk",
+						UserName = "tes234",
+						Email = "usertes2@example.com"
+					},
+				};
+				foreach(var users in userNew)
+				{
+					var result = await _userManager.CreateAsync(users, "walangkecek123");
+					if (result.Succeeded)
+					{
+						Console.WriteLine($"User {users.UserName} created successfully.");
+					}
+					else
+					{
+						foreach (var error in result.Errors)
+						{
+							Console.WriteLine($"Error creating user {users.UserName}: {error.Description}");
+						}
+					}
+				}
+
+				var imageNew = new List<Image>(){
+					new Image
+					{
+						Path = "https://fastly.picsum.photos/id/12/2500/1667.jpg?hmac=Pe3284luVre9ZqNzv1jMFpLihFI6lwq7TPgMSsNXw2w",
+						CreatedAt = DateTime.Now,
+						UpdatedAt = DateTime.Now,
+					},
+					new Image
+					{
+						Path = "https://fastly.picsum.photos/id/9/5000/3269.jpg?hmac=cZKbaLeduq7rNB8X-bigYO8bvPIWtT-mh8GRXtU3vPc",
+						CreatedAt = DateTime.Now,
+						UpdatedAt = DateTime.Now,
+					},
+					new Image
+					{
+						Path = "https://fastly.picsum.photos/id/7/4728/3168.jpg?hmac=c5B5tfYFM9blHHMhuu4UKmhnbZoJqrzNOP9xjkV4w3o",
+						CreatedAt = DateTime.Now,
+						UpdatedAt = DateTime.Now,
+					},
+					new Image
+					{
+						Path = "https://fastly.picsum.photos/id/28/4928/3264.jpg?hmac=GnYF-RnBUg44PFfU5pcw_Qs0ReOyStdnZ8MtQWJqTfA",
+						CreatedAt = DateTime.Now,
+						UpdatedAt = DateTime.Now,
+					}
+				};
+				var imageRangeAdd = _context.Images.AddRangeAsync(imageNew);
+				if (imageRangeAdd.IsCompletedSuccessfully)
+				{
+					var imageAdd = await _context.SaveChangesAsync();
+					if (imageAdd > 0)
+						Console.WriteLine("Image seeding successfully");
+				}
+				var areaNew = new List<Area>()
+				{
+					new Area
+					{
+						Provinsi = "Jawa Barat",
+						Kota = "Bandung",
+						Kecamatan = "Cimahi Selatan",
+						Latitude = "-6.872",
+						Longitude = "107.542",
+						CreatedAt = DateTime.Now,
+						UpdatedAt = DateTime.Now,
+					},
+					new Area
+					{
+						Provinsi = "Jawa Barat",
+						Kota = "Bandung",
+						Kecamatan = "Cimahi Utara",
+						Latitude = "-6.872",
+						Longitude = "107.542",
+						CreatedAt = DateTime.Now,
+						UpdatedAt = DateTime.Now,
+					},
+					new Area
+					{
+						Provinsi = "Jawa Barat",
+						Kota = "Bandung",
+						Kecamatan = "Cimahi Tengah",
+						Latitude = "-6.872",
+						Longitude = "107.542",
+						CreatedAt = DateTime.Now,
+						UpdatedAt = DateTime.Now,
+					}
+				};
+				var areaRangeAdd = _context.Areas.AddRangeAsync(areaNew);
+				if (areaRangeAdd.IsCompletedSuccessfully)
+				{
+					var areaAdd = await _context.SaveChangesAsync();
+					if (areaAdd > 0)
+						Console.WriteLine("Area seeding successfully");
+				};
+				var commodityTypeNew = new CommodityType
+				{
+					Name = "Sayur",
+					Description = "Sayur atau sayuran merupakan sebutan umum bagi bahan pangan nabati yang biasanya mengandung kadar air yang tinggi, yang dapat dikonsumsi setelah dimasak atau diolah dengan teknik tertentu, atau dalam keadaan segar. Istilah untuk kumpulan berbagai jenis sayur adalah sayur-sayuran atau sayur-mayur.",
+					CreatedAt = DateTime.Now,
+					UpdatedAt = DateTime.Now,
+				};
+				_context.CommodityTypes.Add(commodityTypeNew);
+				var commodityTypeSave = await _context.SaveChangesAsync();
+				if (commodityTypeSave > 0)
+					Console.WriteLine("Commodity Type seeding successfully");
+
+				var user1 = await _userManager.FindByEmailAsync("usertes1@example.com");
+				var user2 = await _userManager.FindByEmailAsync("usertes2@example.com");
+				var image1 = await _context.Images.Where(i => i.Path == "https://fastly.picsum.photos/id/12/2500/1667.jpg?hmac=Pe3284luVre9ZqNzv1jMFpLihFI6lwq7TPgMSsNXw2w").FirstOrDefaultAsync();
+				var image2 = await _context.Images.Where(i => i.Path == "https://fastly.picsum.photos/id/9/5000/3269.jpg?hmac=cZKbaLeduq7rNB8X-bigYO8bvPIWtT-mh8GRXtU3vPc").FirstOrDefaultAsync();
+				var image3 = await _context.Images.Where(i => i.Path == "https://fastly.picsum.photos/id/7/4728/3168.jpg?hmac=c5B5tfYFM9blHHMhuu4UKmhnbZoJqrzNOP9xjkV4w3o").FirstOrDefaultAsync();
+				var image4 = await _context.Images.Where(i => i.Path == "https://fastly.picsum.photos/id/28/4928/3264.jpg?hmac=GnYF-RnBUg44PFfU5pcw_Qs0ReOyStdnZ8MtQWJqTfA").FirstOrDefaultAsync();
+				/*var image2 = await _context.Images.FindAsync("https://fastly.picsum.photos/id/9/5000/3269.jpg?hmac=cZKbaLeduq7rNB8X-bigYO8bvPIWtT-mh8GRXtU3vPc");
+				var image3 = await _context.Images.FindAsync("https://fastly.picsum.photos/id/7/4728/3168.jpg?hmac=c5B5tfYFM9blHHMhuu4UKmhnbZoJqrzNOP9xjkV4w3o");
+				var image4 = await _context.Images.FindAsync("https://fastly.picsum.photos/id/28/4928/3264.jpg?hmac=GnYF-RnBUg44PFfU5pcw_Qs0ReOyStdnZ8MtQWJqTfA");*/
+				var area1 = await _context.Areas.Where(a => a.Kecamatan == "Cimahi Selatan").FirstOrDefaultAsync();
+				var area2 = await _context.Areas.Where(a => a.Kecamatan == "Cimahi Utara").FirstOrDefaultAsync();
+				var area3 = await _context.Areas.Where(a => a.Kecamatan == "Cimahi Tengah").FirstOrDefaultAsync();
+
+				/*var area1 = await _context.Areas.FindAsync("Cimahi Selatan");
+				var area2 = await _context.Areas.FindAsync("Cimahi Utara");
+				var area3 = await _context.Areas.FindAsync("Cimahi Tengah");*/
+				var commodityType1 = await _context.CommodityTypes.Where(c => c.Name == "Sayur").FirstOrDefaultAsync();
+				/*var commodityType1 = await _context.CommodityTypes.FindAsync("Sayur");*/
+
+				var userArea = new List<UserArea>(){
+					new UserArea
+					{
+						User = user1,
+						Area = area1,
+					},
+					new UserArea
+					{
+						User = user1,
+						Area = area2,
+					},
+
+				};
+				var userAreaAdd = _context.UserAreas.AddRangeAsync(userArea);
+				if (userAreaAdd.IsCompletedSuccessfully)
+				{
+					var userAreaSave = await _context.SaveChangesAsync();
+					if (userAreaSave > 0)
+						Console.WriteLine("User Area seeding successfully");
+				}
+				var discussion = new Discussion
+				{
+					Message = "Rina adalah seorang petani yang memiliki lahan pertanian di daerah yang cenderung kering. Setelah musim tanam terakhir yang kurang sukses akibat kurangnya air, ia ingin mencari solusi irigasi yang efisien untuk pertanaman berikutnya. Bagaimana Anda akan memberi saran pada Rina mengenai pilihan sistem irigasi yang sesuai?",
+					Likes = 1,
+					User = user1,
+					IsSolved = false,
+					CreatedAt = DateTime.Now,
+					UpdatedAt = DateTime.Now,
+				};
+				var discussionImage = new DiscussionImage
+				{
+					Image = image1,
+					Discussion = discussion,
+				};
+				_context.DiscussionImages.Add(discussionImage);
+				var discussionImageSave = await _context.SaveChangesAsync();
+					if (discussionImageSave > 0)
+						Console.WriteLine("Discussion Image seeding successfully");
+
+				var discussionAnswer = new DiscussionAnswer
+				{
+					Message = "Tentu, Rina. Karena lahan Anda cenderung kering, penting untuk memilih sistem irigasi yang dapat mengirimkan air secara efisien ke tanaman. Sistem irigasi tetes atau sistem irigasi tetesan adalah pilihan yang baik. Sistem ini menyuplai air langsung ke akar tanaman melalui pipa tetesan kecil, mengurangi kebuangan air karena penguapan atau aliran berlebih. Ini membantu Anda menghemat air dan mengalirkan ke air hanya ke area yang dibutuhkan. Selain itu, mempertimbangkan penggunaan alat monitoring tanah atau sensor kelembaban dapat membantu Anda mengetahui kapan tanaman perlu air tambahan, sehingga Anda dapat mengoptimalkan irigasi.",
+					Likes = 1,
+					AnsweredBy = user2,
+					Discussion = _context.Discussions.Where(d => d.User == user1).FirstOrDefault(),
+					CreatedAt = DateTime.Now,
+					UpdatedAt = DateTime.Now,
+				};
+				_context.DiscussionAnswers.Add(discussionAnswer);
+				var discussionAnswerSave = await _context.SaveChangesAsync();
+					if (discussionAnswerSave > 0)
+						Console.WriteLine("Discussion Answer seeding successfully");
+				
+				var pinnedDiscussionAnswer = new PinnedDiscussionAnswer
+				{
+					Discussion = _context.Discussions.Where(d => d.User == user1).FirstOrDefault(),
+					DiscussionAnswer = _context.DiscussionAnswers.Where(d => d.AnsweredBy == user2).FirstOrDefault(),
+				};
+				_context.PinnedDiscussionAnswers.Add(pinnedDiscussionAnswer);
+				var pinnedDiscussionAnswerSave = await _context.SaveChangesAsync();
+				if (pinnedDiscussionAnswerSave > 0)
+					Console.WriteLine("Pinned Discussion Answer seeding successfully");
+				
+				var discussionAnswerImage = new DiscussionAnswerImage
+				{
+					DiscussionAnswer = _context.DiscussionAnswers.Where(d => d.AnsweredBy == user2).FirstOrDefault(),
+					Image = image2
+				};
+				_context.DiscussionAnswerImages.Add(discussionAnswerImage);
+				var discussionAnswerImageSave = await _context.SaveChangesAsync();
+				if (discussionAnswerImageSave > 0)
+					Console.WriteLine("Discussion Answer Image seeding successfully");
+				
+
+				var notification = new Notification
+				{
+					Message = "Hoaks telah menambahkan diskusi baru",
+					IsRead = false,
+					User = user1,
+					CreatedAt = DateTime.Now,
+					UpdatedAt = DateTime.Now,
+				};
+				_context.Notifications.Add(notification);
+				var notificationSave = await _context.SaveChangesAsync();
+				if (notificationSave > 0)
+					Console.WriteLine("Notification seeding successfully");
+				
+				_context.NotificationImages.Add(new NotificationImage
+				{
+					Notification = _context.Notifications.Where(n => n.User == user1).FirstOrDefault(),
+					Image = image3
+				});
+				var notificationImageSave = await _context.SaveChangesAsync();
+				if (notificationImageSave > 0)
+					Console.WriteLine("Notification Image seeding successfully");
+				
+				var commodity = new Commodity
+				{
+					Name = "Sayur Daun",
+					Description = "Sayuran daun, juga disebut sayuran daun hijau, sayuran hijau, atau sekadar sayuran hijau, adalah daun tanaman yang dimakan sebagai sayuran, terkadang disertai dengan tangkai daun dan pucuk yang empuk. Sayuran daun yang dimakan mentah dalam salad bisa disebut salad greens.", 
+					CommodityType = commodityType1,
+					CreatedAt = DateTime.Now,
+					UpdatedAt = DateTime.Now,
+				};
+				_context.Commodities.Add(commodity);
+				var commoditySave = await _context.SaveChangesAsync();
+				if (commoditySave > 0)
+					Console.WriteLine("Commodity seeding successfully");
+				
+				var commodityArea = new CommodityArea
+				{
+					Commodity = _context.Commodities.Where(c => c.Name == "Sayur Daun").FirstOrDefault(),
+					Area = area2,
+				};
+				_context.CommodityAreas.Add(commodityArea);
+				var commodityAreaSave = await _context.SaveChangesAsync();
+				if (commodityAreaSave > 0)
+					Console.WriteLine("Commodity Area seeding successfully");
+				
+				var product = new Product
+				{
+					Name = "Kangkung",
+					Description = "Kangkung adalah tumbuhan yang termasuk jenis sayur-sayuran dan ditanam sebagai makanan. Kangkung banyak dijual di pasar-pasar. Kangkung banyak terdapat di kawasan Asia, tempat asalnya tidak diketahui. dan merupakan tumbuhan yang dapat dijumpai hampir di mana-mana terutama di kawasan berair.",
+					PriceUnit = "Kg",
+					Price = 10000,
+					Stock = 100,
+					Commodity = _context.Commodities.Where(c => c.Name == "Sayur Daun").FirstOrDefault(),
+					Area = area2,
+					User = user1,
+					Status = 1,
+					CreatedAt = DateTime.Now,
+					UpdatedAt = DateTime.Now,
+				};
+				_context.Products.Add(product);
+				var productSave = await _context.SaveChangesAsync();
+				if (productSave > 0)
+					Console.WriteLine("Product seeding successfully");
+
+			var productImage = new ProductImage
+			{
+					Product = _context.Products.Where(p => p.Name == "Kangkung").FirstOrDefault(),
+					Image = image4
+				};
+				_context.ProductImages.Add(productImage);
+				var productImageSave = await _context.SaveChangesAsync();
+				if (productImageSave > 0)
+					Console.WriteLine("Product Image seeding successfully");
+				
+				var order = new Order
+				{
+					Product = _context.Products.Where(p => p.Name == "Kangkung").FirstOrDefault(),
+					Buyer = user2,
+					Quantity = 10,
+					Notes = "Yang bagus-bagus mas",
+					Status = "Dikemas",
+					CreatedAt = DateTime.Now,
+					UpdatedAt = DateTime.Now,
+				};
+				_context.Orders.Add(order);
+				var orderSave = await _context.SaveChangesAsync();
+				if (orderSave > 0)
+					Console.WriteLine("Order seeding successfully");
+				
+				var predictionNew = new List<Prediction>()
+				{
+					new Prediction
+					{
+						Price = 1000000,
+						Date = DateTime.Now.AddDays(20),
+						Commodity = _context.Commodities.Where(c => c.Name == "Sayur Daun").FirstOrDefault(),
+						Area = area3,
+						CreatedAt = DateTime.Now,
+						UpdatedAt = DateTime.Now,
+					},
+					new Prediction
+					{
+						Price = 2000000,
+						Date = DateTime.Now.AddDays(20),
+						Commodity = _context.Commodities.Where(c => c.Name == "Sayur Daun").FirstOrDefault(),
+						Area = area2,
+						CreatedAt = DateTime.Now,
+						UpdatedAt = DateTime.Now,
+					},
+					new Prediction
+					{
+						Price = 3000000,
+						Date = DateTime.Now.AddDays(20),
+						Commodity = _context.Commodities.Where(c => c.Name == "Sayur Daun").FirstOrDefault(),
+						Area = area1,
+						CreatedAt = DateTime.Now,
+						UpdatedAt = DateTime.Now,
+					},
+				};
+				var predictionAdd = _context.Predictions.AddRangeAsync(predictionNew);
+				if (predictionAdd.IsCompletedSuccessfully)
+				{
+					var predictionSave = await _context.SaveChangesAsync();
+					if (predictionSave > 0)
+						Console.WriteLine("Prediction seeding successfully");
+				}
+			/*}
+			else
+			{
+				Console.WriteLine("Already seeded");
+			}*/
+		}
+    }
 }
