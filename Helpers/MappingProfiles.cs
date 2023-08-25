d5fcc1bb99d901c06b57b23a46c178c2d947cd6d
hubedid <hubed@protonmail.com>
2023-08-25 21:58:18 +0700

    [ADD] Mapping profile for Discussion, Answer, Image

--- Helpers/MappingProfiles.cs
+++ Helpers/MappingProfiles.cs
@@ -1,4 +1,5 @@
 ﻿using A_GroTech_Api.Dto;
+using A_GroTech_Api.Dto.BodyModels;
 using A_GroTech_Api.Models;
 using AutoMapper;
 
@@ -9,6 +10,18 @@ namespace A_GroTech_Api.Helpers
         public MappingProfiles()
         {
             CreateMap<User, UserDto>();
+            CreateMap<Discussion, DiscussionDto>()
+                .ForMember(dest => dest.CreatedBy, options => options.MapFrom(src => src.User));
+			CreateMap<DiscussionPostDto, Discussion>();
+			CreateMap<DiscussionPutDto, Discussion>();
+			CreateMap<DiscussionAnswer, DiscussionAnswerDto>()
+                .ForMember(dest => dest.AnsweredBy, options => options.MapFrom(src => src.AnsweredBy))
+                .ForMember(dest => dest.DiscussionId, options => options.MapFrom(src => src.Discussion.Id));
+			CreateMap<DiscussionAnswerPostDto, DiscussionAnswer>();
+			CreateMap<DiscussionAnswerPutDto, DiscussionAnswer>();
+			CreateMap<Area, AreaDto>();
+            CreateMap<Image, ImageDto>();
+			CreateMap<ImagePostDto, Image>();
         }
     }
 }
