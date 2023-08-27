a68b85649d3407c4185f80166a42d4fd3cc37d2a
hubedid <hubed@protonmail.com>
2023-08-27 16:41:19 +0700

    [ADD] all dto mapping profile

--- Helpers/MappingProfiles.cs
+++ Helpers/MappingProfiles.cs
@@ -23,6 +23,32 @@ namespace A_GroTech_Api.Helpers
             CreateMap<AreaPostDto, Area>();
             CreateMap<Image, ImageDto>();
 			CreateMap<ImagePostDto, Image>();
+            CreateMap<CommodityType, CommodityTypeDto>();
+            CreateMap<CommodityTypePostDto, CommodityType>();
+            CreateMap<Commodity, CommodityDto>();
+            CreateMap<CommodityPostDto, Commodity>();
+            CreateMap<CommodityPutDto, Commodity>();
+            CreateMap<Prediction, PredictionDto>()
+                .ForMember(dest => dest.CommodityId, options => options.MapFrom(src => src.Commodity.Id))
+				.ForMember(dest => dest.AreaId, options => options.MapFrom(src => src.Area.Id));
+            CreateMap<PredictionPostDto, Prediction>();
+            CreateMap<Notification, NotificationDto>();
+            CreateMap<NotificationPostDto, Notification>();
+            CreateMap<NotificationPutDto, Notification>();
+            CreateMap<Product, ProductDto>()
+                .ForMember(dest => dest.CommodityId, options => options.MapFrom(src => src.Commodity.Id))
+                .ForMember(dest => dest.AreaId, options => options.MapFrom(src => src.Area.Id))
+                .ForMember(dest => dest.OwnerId, options => options.MapFrom(src => src.User.Id));
+            CreateMap<ProductPostDto, Product>();
+            CreateMap<ProductPutDto, Product>();
+            CreateMap<ProductReview, ProductReviewDto>()
+                .ForMember(dest => dest.ReviewedBy, options => options.MapFrom(src => src.ReviewedBy))
+                .ForMember(dest => dest.ProductId, options => options.MapFrom(src => src.Product.Id));
+            CreateMap<ProductReviewPostDto, ProductReview>();
+            CreateMap<ProductReviewPutDto, ProductReview>();
+            CreateMap<Order, OrderDto>()
+                .ForMember(dest => dest.Buyer, options => options.MapFrom(src => src.Buyer))
+                .ForMember(dest => dest.ProductId, options => options.MapFrom(src => src.Product.Id));
         }
     }
 }
