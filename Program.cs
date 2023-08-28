63e8133018f5c5ed3289302aa24b664d3424b843
hubedid <hubed@protonmail.com>
2023-08-28 16:44:49 +0700

    [ADD] Docker support configuration

--- Program.cs
+++ Program.cs
@@ -140,7 +140,7 @@ if (app.Environment.IsDevelopment())
 	app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
 app.UseAuthentication();
 /*app.UseExceptionHandler(errorApp =>
